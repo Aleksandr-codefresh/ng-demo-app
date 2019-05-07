31b753230ec33cb23d38cd908bc14bd632a4c68a
Aleksandr-codefresh <a.pototskiy@codefresh.io>
2019-05-07 14:56:53 +0300

    Add ingredients to recipies

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Tomatos', 2),
     new Ingredient('Apples', 5)
@@ -17,5 +18,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
   }
 }
