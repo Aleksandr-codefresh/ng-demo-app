ec8d42e2180d7d9be9ce589c4616c4e5cb26ac32
Aleksandr-codefresh <a.pototskiy@codefresh.io>
2019-05-11 23:32:53 +0300

    Edit recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   {
@@ -15,7 +16,13 @@ const appRoutes: Routes = [
         path: '', component: RecipeStartComponent, pathMatch: 'full'
       },
       {
+        path: 'new', component: RecipeEditComponent
+      },
+      {
         path: ':id', component: RecipeDetailComponent
+      },
+      {
+        path: ':id/edit', component: RecipeEditComponent
       }
     ]
   },
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,16 +1,20 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.module';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Tomatos', 2),
     new Ingredient('Apples', 5)
   ];
-  constructor() { }
+
+  get ingredientsChangedObservable(): Observable<Ingredient[]> {
+    return this.ingredientsChanged.asObservable();
+  }
 
   getIngredients(): Ingredient[] {
     return [...this.ingredients];
@@ -18,11 +22,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 
   addIngredients(ingredients: Ingredient[]): void {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 }
