003ec8b079c63b4964bce2da8a9c2c05839dd744
Alex <a.pototskiy@codefresh.io>
2020-01-18 18:56:19 +0300

    Create recipies module

---
+++ src/app/recipes/recipies.module.ts
@@ -0,0 +1,40 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule,
+        // FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ],
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+    ],
+    providers: [],
+})
+export class RecipiesModule { }
